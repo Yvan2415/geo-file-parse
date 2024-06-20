7772b6fdee75408ddade9ef5cbab1e534a8d9f9e
Yvan2415 <fang_gryl@163.com>
2024-06-20 23:48:27 +0800

    fix: 修改打包结果, 新增amd 和 es

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,40 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import nodePolyfills from 'rollup-plugin-node-polyfills'
 import json from '@rollup/plugin-json'
+import terser from '@rollup/plugin-terser'
 
 
-export default {
-  input: './lib/index.js',
-  output: {
-    file: './dist/index.js',
-    format: 'umd',
-    name: 'fileReader',
-  },
-  plugins: [
-    json(),
-    nodeResolve(),
-    commonjs(),
-    nodePolyfills(),
-  ]
-}
+export default [
+  {
+    input: './lib/index.js',
+    output: {
+      file: './dist/index.min.js',
+      format: 'amd',
+      name: 'fileReader',
+    },
+    plugins: [
+      json(),
+      nodeResolve(),
+      commonjs(),
+      nodePolyfills(),
+      terser()
+    ]
+  }, {
+    input: './lib/index.js',
+    output: {
+      file: './dist/index.js',
+      format: 'es'
+    },
+    external: [
+      'jszip',
+      'shpjs',
+      '@mapbox/togeojson'
+    ],
+    plugins: [
+      json(),
+      nodeResolve(),
+      commonjs(),
+      nodePolyfills(),
+    ]
+  }
+]
